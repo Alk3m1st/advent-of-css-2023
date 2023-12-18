356e10c015819d7b5b7e19abea043cae4bec7ce8
Allan Oliveira <a.oliveira@curtin.edu.au>
2023-12-18 17:31:33 +0800

    feat: Added form field for password with visibility toggle

--- src/components/FormField.tsx
+++ src/components/FormField.tsx
@@ -1,8 +1,32 @@
-export const FormField = () => {
+import { useState } from "react";
+import { Icon } from "./Icon";
+
+type FormFieldProps = {
+  label: string;
+  name: string;
+};
+
+export const FormField = ({ label, name, ...rest }: FormFieldProps) => {
+  const [isPasswordShowing, setIsPasswordShowing] = useState(false);
+
+  const toggle = () => {
+    setIsPasswordShowing((prevValue) => !prevValue);
+  };
+
   return (
-    <label>
-      Field
-      <input className="max-w-lg h-10" type="text" />
-    </label>
+    <div className="relative">
+      <label htmlFor="password">{label}</label>
+      <input
+        type={isPasswordShowing ? "text" : "password"}
+        id={name}
+        name={name}
+        placeholder=""
+        required
+        {...rest}
+      />
+      <button className="absolute top-8 right-6" onClick={() => toggle()}>
+        <Icon id={isPasswordShowing ? "eyeOpened" : "eyeClosed"} size={32} />
+      </button>
+    </div>
   );
 };
--- src/stories/FormField.stories.ts
+++ src/stories/FormField.stories.ts
@@ -12,38 +12,18 @@ const meta = {
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ["autodocs"],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
-  //   argTypes: {
-  //     avatar: { control: "text" },
-  //     icon: {
-  //       control: "select",
-  //       options: iconIds,
-  //     },
-  //     letter: { control: "text" },
-  //     isSantaHatShowing: { control: "boolean" },
-  //     // size: { control: { type: "range", min: 8, max: 128, step: 2 } },
-  //   },
 } satisfies Meta<typeof FormField>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const EmptyName: Story = {
-  //   args: {
-  //     avatar: AvatarImage01,
-  //     alt: "Avatar image",
-  //   },
-};
-
-export const FilledName: Story = {
-  //   args: {
-  //     letter: "A",
-  //   },
-};
-
-export const EmailAddress: Story = {
-  //   args: {
-  //     icon: "eyeClosed",
-  //   },
+export const Password: Story = {
+  args: {
+    label: "Password",
+    name: "password",
+  },
+  // render: () => (
+  //   <><form><FormField /></form></>
+  // ),
 };
